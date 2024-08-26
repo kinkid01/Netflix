6e7bdf6d107c81dd79683201d96012f8247d48f6
michaelayegbamartins4real@gmail.com <michaelayegbamartins4real@gmail.com>
2024-08-26 19:08:40 +0100

    just another netflix project

---
+++ src/Nav.js
@@ -0,0 +1,37 @@
+import "./Nav.css";
+import { useEffect, useState } from "react";
+
+const Nav = () => {
+  const [show, handleShow] = useState(false);
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <div className="nav_content ">
+        <img
+          className="nav_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/f/fd/Netflix-Logo.png"
+          alt=""
+        />
+        <img
+          className="nav_avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
---
+++ src/Row.js
@@ -0,0 +1,47 @@
+import axios from "./axios";
+import React, { useEffect, useState } from "react";
+import "./Row.css";
+
+const Row = ({ title, fetchUrl, isLargeRow = false }) => {
+  const [movies, setMovies] = useState([]);
+
+  const base_url = "https://image.tmdb.org/t/p/original/";
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const request = await axios.get(fetchUrl);
+        setMovies(request.data.results);
+        return request;
+      } catch (error) {
+        if (error.request.status === 401) {
+          console.error("Unauthorized: Please check your credentials.");
+        } else {
+          console.error("An error occurred:", error.message);
+        }
+      }
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            key={movie.id}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
